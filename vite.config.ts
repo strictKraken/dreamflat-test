fa8f5da05099bb7e36f732a3ddf1868ce194005b
StrictKraken <StefanStark77@gmail.com>
2023-12-15 19:50:57 +0300

    Init application

---
+++ vite.config.ts
@@ -0,0 +1,31 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig, loadEnv } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+import Components from "unplugin-vue-components/vite";
+import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
+
+export default defineConfig(({ _, mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    base: env.BASE_URL,
+    plugins: [
+      // analyzer(),
+      vue(),
+      Components({
+        resolvers: [
+          AntDesignVueResolver({
+            importStyle: false
+          })
+        ]
+      })
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url))
+      }
+    }
+  };
+});
