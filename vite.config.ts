47bfb1b68322180c9a4cc3397f7996e75e188e4d
StrictKraken <StefanStark77@gmail.com>
2023-12-16 19:37:27 +0300

    fix: vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vue from "@vitejs/plugin-vue";
 import Components from "unplugin-vue-components/vite";
 import { AntDesignVueResolver } from "unplugin-vue-components/resolvers";
 
-export default defineConfig(({ _, mode }) => {
+export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
